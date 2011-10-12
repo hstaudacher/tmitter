7b63faa205cf17ccbf5e8bee938114c79f2088ff
hstaudacher <hstaudacher@eclipsesource.com>
2011-10-12 21:07:47 +0200

    Updated Monster Creator

--- tmitter.model/src/tmitter/model/MonsterCreator.java
+++ tmitter.model/src/tmitter/model/MonsterCreator.java
@@ -10,35 +10,67 @@ public class MonsterCreator {
     herman.setPassword( "munster" );
     herman.setSpecies( Species.ZOMBIE );
     
-    herman.updateStatus( "My first Status" );
-    sleep();
-    herman.updateStatus( "My second Status" );
-    sleep();
-    herman.updateStatus( "My third Status" );
-    sleep();
+    Monster lily = new Monster( "lily" );
+    lily.setPassword( "munster" );
+    lily.setSpecies( Species.VAMPIRE );
     
-    Monster lilli = new Monster( "lily" );
-    lilli.updateStatus( "I love @herman :)" );
-    sleep();
-    herman.updateStatus( "I know..." );
-    sleep();
     Monster grandpa = new Monster( "grandpa" );
-    grandpa.updateStatus( "I'm the count" );
+    grandpa.setPassword( "munster" );
+    grandpa.setSpecies( Species.VAMPIRE );
+    
+    Monster eddie = new Monster( "eddie" );
+    eddie.setPassword( "munster" );
+    eddie.setSpecies( Species.WEREWOLF );
+    
+    lily.updateStatus( "Oh dear, this is a network for us ;)" );
     sleep();
-    lilli.save();
+    grandpa.updateStatus( "@lily it seems so." );
+    sleep();
+    lily.updateStatus( "@grandpa we have to invite herman and eddie" );
+    sleep();
+    eddie.updateStatus( "@lily @grandpa no need. i found my way to Tmitter" );
+    sleep();
+    eddie.updateStatus( "Only dad misses this fun" );
+    sleep();
+    herman.updateStatus( "Nope. Just because I consist of ten men does not mean I can't tmeet" );
+    sleep();
+    grandpa.updateStatus( "Welcome @herman to Tmitter" );
+    sleep();
+    eddie.updateStatus( "@herman hi dad, I'm going to do my homework now" );
+    sleep();
+    herman.updateStatus( "Such a god baby dog" );
+    sleep();
+    lily.updateStatus( "@herman Hi my dear. Can you bring some poisen from the market?" );
+    System.out.println( "Created tmeets" );
+    sleep();
+    
+    herman.follow( "grandpa" );
+    herman.follow( "lily" );
+    
+    grandpa.follow( "lily" );
+    grandpa.follow( "grandpa" );
+    grandpa.follow( "herman" );
+    
+    eddie.follow( "herman" );
+    eddie.follow( "lily" );
+    
+    lily.follow( "grandpa" );
+    lily.follow( "herman" );
+    lily.follow( "eddie" );
+    
+    System.out.println( "Added followers" );
+    
+    lily.save();
     grandpa.save();
-    
-    herman.follow( lilli.getName() );
-    herman.follow( grandpa.getName() );
-    
     herman.save();
-    
-    Monster loadedHurman = MonsterUtil.loadMonster( "herman" );
-    System.out.println( loadedHurman );
+    eddie.save();
+    System.out.println( "Saved monsters" );
+    System.out.println( "--- DONE ---" );
   }
   
   private static void sleep() {
     try {
+      System.out.println( "Creating tmeet" );
       Thread.sleep( 1000 );
     } catch( InterruptedException e ) {
       e.printStackTrace();
